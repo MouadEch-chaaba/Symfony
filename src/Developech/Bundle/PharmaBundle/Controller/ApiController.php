d57da5244d65012fcf47f88fe2d68a939246961a
Mouad <mouad.echchaaba@gmail.com>
2015-10-21 01:19:54 +0000

    Testing the JsonResponse object

--- src/Developech/Bundle/PharmaBundle/Controller/ApiController.php
+++ src/Developech/Bundle/PharmaBundle/Controller/ApiController.php
@@ -3,12 +3,13 @@
 namespace Developech\Bundle\PharmaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends Controller
 {
     public function defaultAction($id)
     {
-        return new Response(json_encode(array("ID" => $id)),200,array("Content-type" => "application/json"));
+        return new JsonResponse(array("ID" => $id));
     }
 }
