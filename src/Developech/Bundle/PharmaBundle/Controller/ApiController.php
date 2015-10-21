b542dd4415f0fccb20e964f26da5719fd9b8da5e
Mouad <mouad.echchaaba@gmail.com>
2015-10-21 10:07:11 +0000

    Adding PhpDocs to the default controller

--- src/Developech/Bundle/PharmaBundle/Controller/ApiController.php
+++ src/Developech/Bundle/PharmaBundle/Controller/ApiController.php
@@ -4,10 +4,13 @@ namespace Developech\Bundle\PharmaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends Controller
 {
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
     public function defaultAction($id)
     {
         return new JsonResponse(array("ID" => $id));
