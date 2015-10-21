13ed2eda2b814066a460e12d57ea96a175b318ec
Mouad <mouad.echchaaba@gmail.com>
2015-10-21 21:19:30 +0000

    Testing somme Symfony Core features {Commit to be ignored}

--- src/Developech/Bundle/PharmaBundle/Controller/DefaultController.php
+++ src/Developech/Bundle/PharmaBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace Developech\Bundle\PharmaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name,Request $request)
     {
-        return $this->render('DevelopechPharmaBundle:Default:index.html.twig', array('name' => $name));
+        $age = $request->get("age");
+        return $this->render('DevelopechPharmaBundle:Default:index.html.twig', array(
+            'name' => $name,
+            'age' => $age
+        ));
     }
 }
