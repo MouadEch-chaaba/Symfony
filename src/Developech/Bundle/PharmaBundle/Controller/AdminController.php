62a4b002299a5b8b4fdfd05b49895ba348ff9b65
Mouad <mouad.echchaaba@gmail.com>
2015-10-25 13:24:20 +0000

    Fixing a bug in Admin controller {homeAction}

--- src/Developech/Bundle/PharmaBundle/Controller/AdminController.php
+++ src/Developech/Bundle/PharmaBundle/Controller/AdminController.php
@@ -9,6 +9,6 @@ class AdminController extends Controller
 {
     public function homeAction()
     {
-        return $this.$this->render("DevelopechPharmaBundle:Admin:home.html.twig");
+        return $this->render("DevelopechPharmaBundle:Admin:home.html.twig");
     }
 }
